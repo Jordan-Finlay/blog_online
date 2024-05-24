f4db9ddc6b0afaa2a076d9d2ce11acef6976544a
Jordan Finlay <jfinlay@hotmail.co.uk>
2024-05-24 13:24:18 +0000

    updated 403 error now fixed

--- my_project/settings.py
+++ my_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,9 @@ SECRET_KEY = 'django-insecure-9k%$fn(@%jg87s^chr@v*!@g33a1&p95!g3j^65asyv)#!6@=v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
 ALLOWED_HOSTS = ['8000-jordanfinlay-blogonline-ldfvserosbi.ws-eu114.gitpod.io','.herokuapp.com']
+CSRF_TRUSTED_ORIGINS = ['https://8000-jordanfinlay-blogonline-ldfvserosbi.ws-eu114.gitpod.io']
 
 # Application definition
 
--- personal/views.py
+++ personal/views.py
@@ -4,9 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home_screen(request):
-
     context = {}
     context['tester_string'] = 'This is a test to input a string'
 
     return render(request, 'personal/home.html', context)
-
