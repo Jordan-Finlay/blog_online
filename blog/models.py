892eb261fa955313185fdf9233263795cd43acf0
Jordan Finlay <jfinlay@hotmail.co.uk>
2024-06-10 07:05:20 +0000

    small changes on blog

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-from django.db.models.signals import pre_save
 from django.utils.text import slugify
 from django.conf import settings
-from django.db.models.signals import post_delete
+from django.db.models.signals import post_delete, pre_save
 from django.dispatch import receiver
 
-def upload_location(instance, filename):
-    file_path = 'blog/{author_id}/{title}-{filename}'.format{
-        author_id=str(instance.author.id), title=str(instance.title), filmename=filemane
-    }
+#Takes the upload location
+def upload_location(instance, filename, **kwargs):
+    file_path = 'blog/{author_id}/{title}-{filename}'.format(
+        author_id=str(instance.author.id), title=str(instance.title), filename=filename
+    )
     return file_path
 
 class BlogPost(models.Model):
@@ -23,7 +23,14 @@ class BlogPost(models.Model):
     def __str__(self):
         return self.title
 
+#Deletes image if blogpost is deleted
+@receiver(post_delete, sender=BlogPost)
+def submission_delete(sender, instance, **kwargs):
+    instance.image.delete(False)
 
+#pre save 
+def pre_save_blog_post_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = slugify(instance.author.username + "-" + instance.title)
 
-
-
+pre_save.connect(pre_save_blog_post_receiver, sender=BlogPost)
