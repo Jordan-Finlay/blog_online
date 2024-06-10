d2f6d15651548f802b77b828a523132e65c7b964
Jordan Finlay <jfinlay@hotmail.co.uk>
2024-06-10 10:08:06 +0000

    added static files changed background style for header

--- my_project/urls.py
+++ my_project/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from about import views as about_views
 from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 from personal.views import home_screen
 from account.views import ( 
@@ -52,3 +54,7 @@ urlpatterns = [
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
      name='password_reset_complete'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
