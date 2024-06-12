e6de76c2cad3924084eb3840440623f95e9d7e93
Jordan Finlay <jfinlay@hotmail.co.uk>
2024-06-12 11:33:44 +0000

    updated shown blog posts. added a link to such posts

--- blog/urls.py
+++ blog/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from blog.views import (
 
     create_blog_view,
+    detail_blog_view,
 )
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('create/', create_blog_view, name="create"),
+    path('create/', create_blog_view, name="create"),#Sends user to create post
+    path('<slug>/', detail_blog_view, name="detail"),#Sends user to post they created
 ]
