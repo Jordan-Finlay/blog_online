16317b75c89dca6ea6fb80d7fabbb56ca906a51f
Jordan Finlay <jfinlay@hotmail.co.uk>
2024-06-12 12:33:19 +0000

    added function to check author and allow editting of posts/images

--- blog/forms.py
+++ blog/forms.py
@@ -8,3 +8,26 @@ class CreateBlogPostForm(forms.ModelForm):
     class Meta:
         model = BlogPost
         fields = ['title', 'body', 'image']
+
+
+#Allows users to edit their posts
+class UpdateBlogPostForm(forms.ModelForm):
+
+    class Meta:
+        model = BlogPost
+        fields = ['title', 'body', 'image']
+
+    #Finds the title/body
+    def save(self, commit=True):
+        blog_post = self.instance
+        blog_post.title = self.cleaned_data['title']
+        blog_post.body = self.cleaned_data['body']
+
+        #Finds the image
+        if self.cleaned_data['image']:
+            blog_post.image = self.cleaned_data['image']
+
+        #If committed - Updates the post
+        if commit:
+            blog_post.save()
+        return blog_post
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
 from blog.models import BlogPost
-from blog.forms import CreateBlogPostForm
+from blog.forms import CreateBlogPostForm, UpdateBlogPostForm
 from account.models import Account
 
 def create_blog_view(request):
 
     context = {}
-    
+
+    #Checks if user is authenticated
     user = request.user
-    if not user.is_authenticated:#Checks if user is authenticated
+    if not user.is_authenticated:
         return redirect('must_authenticate')
     
     #Checks author, then saves and reloads
@@ -34,3 +35,34 @@ def detail_blog_view(request, slug):
     context['blog_post'] = blog_post
 
     return render(request, 'blog/detail_blog.html', context)
+
+
+def edit_blog_view(request, slug):
+
+    context = {}
+
+    #Checks if the user is authenticated before editing post
+    user = request.user
+    if not user.is_authenticated:
+        return redirect("must_authenticate")
+
+    #Updates and shows a success message
+    blog_post = get_object_or_404(BlogPost, slug=slug)
+    if request.POST:
+        form = UpdateBlogPostForm(request.POST or None, request.FILES or None, instance=blog_post)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.save()
+            context['success_message'] = "Updated"
+            blog_post = obj
+
+    form = UpdateBlogPostForm(
+        initial = {
+            "title": blog_post.title,
+            "body": blog_post.body,
+            "image": blog_post.image,
+        }
+    )
+
+    context['form'] = form
+    return render(request, 'blog/edit_blog.html', context)
