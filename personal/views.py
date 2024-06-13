96a9ec7def1cbc28c96b2be6e00d51b23404abaa
Jordan Finlay <jfinlay@hotmail.co.uk>
2024-06-13 10:41:51 +0000

    made pagination set to 2 per page atm

--- personal/views.py
+++ personal/views.py
@@ -1,8 +1,12 @@
 from operator import attrgetter
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from blog.models import BlogPost
 from blog.views import get_blog_queryset
 from django.http import HttpResponse
+
+
+BLOG_POSTS_PER_PAGE = 3
 
 
 def home_screen(request): 
@@ -11,11 +15,25 @@ def home_screen(request):
     #If theres a GET request - searches for q then passes it to template
     query = ""
     if request.GET:
-        query = request.GET['q']
+        query = request.GET.get('q', "")
         context['query'] = str(query)
 
     #Arranges newest posts at start/top of feed
     blog_posts = sorted(get_blog_queryset(query), key=attrgetter('date_updated'), reverse=True)
+    context['blog_posts'] = blog_posts
+
+
+    #pagination
+    page = request.GET.get('page', 1)
+    blog_posts_paginator = Paginator(blog_posts, BLOG_POSTS_PER_PAGE)
+
+    try:
+        blog_posts = blog_posts_paginator.page(page)
+    except PageNotAnInteger:
+        blog_posts = blog_posts_paginator.page(BLOG_POSTS_PER_PAGE)
+    except EmptyPage:
+        blog_posts = blog_posts_paginator.page(blog_posts_paginator.num_pages)
+
     context['blog_posts'] = blog_posts
 
     return render(request, 'personal/home.html', context)
