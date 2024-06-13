3625e76f94e038cea8b01b7d0b9cd7e861f62182
Jordan Finlay <jfinlay@hotmail.co.uk>
2024-06-13 06:23:14 +0000

    updated feed page layout/display to look more professional

--- personal/views.py
+++ personal/views.py
@@ -1,12 +1,13 @@
+from operator import attrgetter
 from django.shortcuts import render
-from account.models import Account
+from blog.models import BlogPost
 from django.http import HttpResponse
 
 
 def home_screen(request): 
     context = {}
-
-    accounts = Account.objects.all()
-    context['accounts'] = accounts
+    #Arranges newest posts at start/top of feed
+    blog_posts = sorted(BlogPost.objects.all(), key=attrgetter('date_updated'), reverse=True)
+    context['blog_posts'] = blog_posts
 
     return render(request, 'personal/home.html', context)
